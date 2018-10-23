d63601dc163b76e8d84c73c1cf29645c7d524553
SBriscoe2 <shabirs@tiscali.co.uk>
2018-10-23 22:18:03 +0100

    extracted header, footer and iteration into methods

--- directory.rb
+++ directory.rb
@@ -1,4 +1,3 @@
-student_count = 11
 #let's put all students into an array
 students = [
   "Dr. Hannibal Lecter",
@@ -13,13 +12,19 @@ students = [
   "Joffrey Baratheon",
   "Norman Bates"
 ]
-# and then print them
+def print_header
 puts "The students of Villians Academy"
 puts " -------------------------------"
-students.each do |student|
-  puts student
 end
-# finally, we print the total
-puts "Overall, we have #{student.count} great students"
-
-
+def print(names)
+  names.each do |name|
+    puts name
+  end
+end
+def print_footer(names)
+puts "Overall, we have #{names.count} great students"
+end
+#nothing happens until we call the methods
+print_header
+print(students)
+print_footer(students)
